168b293aa369f70e30883cedaa35ecb335f1e5be
jmoggridge <ja.moggridge@gmail.com>
2023-03-28 23:52:40 -0700

    copy fig to dropbx; add seed to analyis

--- R/copy_reports_to_dropbox.R
+++ R/copy_reports_to_dropbox.R
@@ -3,6 +3,11 @@
 # Copies files to dropbox/CUBE_Study/Data Analysis folder
 # so that other team members can stay up to date.
 #
+# Copy figures and results to .../uOttawa paper/
+
+library(tidyverse)
+
+# copy reports to dropbox
 rmarkdown::render(
   input = 'cube_uottawa_report.Rmd',
   output_dir = '~/Dropbox/CUBE Study/Data Analysis/'
@@ -12,3 +17,19 @@ rmarkdown::render(
   input = 'cube_uottawa_eda.Rmd',
   output_dir = '~/Dropbox/CUBE Study/Data Analysis/'
 )
+
+# copy figures to dropbox
+fs::dir_info(here::here('fig')) |>
+  select(path) |>
+  filter(str_detect(path, '/figure_')) |>
+  mutate(
+    file = str_extract(path, '([^/]+$){1}'),
+    db = '~/Dropbox/CUBE Study/uOttawa paper/figures',
+    new = str_glue('{db}/{file}')
+    ) |>
+  select(path, new) |>
+  mutate(exec = walk2(
+    path, new,
+    ~fs::file_copy(.x, .y, overwrite = T)
+    ))
+
